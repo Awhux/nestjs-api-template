4f254552e52cdf0049dc2e6da27e36c7ea5d7b41
Awhux <brudoismilequatro@gmail.com>
2024-11-14 11:50:04 -0300

    chore: add editorconfig, update eslint and prettier configs, add swcrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,111 @@ module.exports = {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
+    ecmaVersion: 2020,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint', 'import', 'prettier', 'unicorn', 'sonarjs'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:unicorn/recommended',
+    'plugin:sonarjs/recommended-legacy',
+    'prettier',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
+    es6: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    // TypeScript specific rules
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        functions: false,
+        classes: true,
+        variables: true,
+        typedefs: true,
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-unnecessary-condition': 'error',
+    '@typescript-eslint/prefer-nullish-coalescing': 'error',
+    '@typescript-eslint/prefer-optional-chain': 'error',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+
+    // Import rules
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    'import/no-duplicates': 'error',
+
+    // General code quality rules
+    'no-console': 'warn',
+    eqeqeq: ['error', 'always', { null: 'ignore' }],
+    curly: ['error', 'all'],
+    'no-unused-expressions': 'error',
+    'no-eval': 'error',
+    'no-implied-eval': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'prefer-template': 'error',
+    'no-param-reassign': 'error',
+    'no-multi-spaces': 'error',
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+    'padded-blocks': ['error', 'never'],
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        ignoreComments: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+
+    // SonarJS rules
+    'sonarjs/no-duplicate-string': 'error',
+    'sonarjs/cognitive-complexity': ['error', 15],
+
+    // Unicorn rules
+    'unicorn/filename-case': ['error', { case: 'kebabCase' }],
+    'unicorn/prefer-node-protocol': 'error',
+    'unicorn/prefer-module': 'error',
+
+    // Prettier
+    'prettier/prettier': 'error',
   },
-};
+  settings: {
+    // 'import/resolver': {
+    //   typescript: {},
+    // },
+  },
+}
